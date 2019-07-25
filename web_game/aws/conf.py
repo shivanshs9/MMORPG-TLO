3fbd7d9db78b8a20e61b0f6c5a46fe8c09b5abaa
Shivansh Saini <shivanshs9@gmail.com>
2019-07-25 12:21:04 +0530

    S3 fixes

--- web_game/aws/conf.py
+++ web_game/aws/conf.py
@@ -7,11 +7,14 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY', '')
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'web_game.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'web_game.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'mmorpg-tlo'
-S3DIRECT_REGION = 'us-west-2'
+AWS_S3_HOST = 's3.ap-south-1.amazonaws.com'
+S3_USE_SIGV4 = True
+AWS_S3_REGION_NAME = 'ap-south-1'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
